2b8fc987ee1ff05fc95560d09dd94d63acd42fc1
kjh <soskjh5382@naver.com>
2025-06-28 16:44:33 +0900

    chagne cerate-secret

--- xinfra/istory-aws-ec2-single/s3.tf
+++ xinfra/istory-aws-ec2-single/s3.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "istory-deploy-bucket" {
   bucket = "istory-deploy-bucket-${data.aws_caller_identity.current.account_id}"  # 고유한 버킷 이름 필요
 }
 
-# S3 버킷 버전 관리  설정
+# S3 버킷 버전 관리 설정
 resource "aws_s3_bucket_versioning" "deploy_bucket_versioning" {
   bucket = aws_s3_bucket.istory-deploy-bucket.id
   versioning_configuration {
